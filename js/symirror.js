cc7ebcf89889b1a0bf694203cd3d324c25a27c5b
Philipp Marmet <philipp.marmet@adfinis-sygroup.ch>
2015-09-29 12:04:42 +0200

    Added fedora mirror

--- js/symirror.js
+++ js/symirror.js
@@ -58,6 +58,11 @@ var MIRRORS = [
         statisticsUrl:     '/debian.json',
         sizeElement:       'mirror-debian-size',
         timestampElement:  'mirror-debian-lastsync'
+    },
+    {
+        statisticsUrl:     '/fedora.json',
+        sizeElement:       'mirror-fedora-size',
+        timestampElement:  'mirror-fedora-lastsync'
     }
 ]
 
