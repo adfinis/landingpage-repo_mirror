849874650bae8f3747a8537a5334afb5618fe837
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-02-21 11:02:06 +0100

    feat: use tailwinds typography for styling markdown

--- solid/src/components/repo.jsx
+++ solid/src/components/repo.jsx
@@ -50,7 +50,7 @@ export default function Repo(props) {
     return (
         <div class="w-full flex bg-gray-200 rounded transition-all">
             <a
-                class="flex hover:bg-gray-300 p-2 pr-5 rounded-s flex-grow-[6] flex-shrink transition-all"
+                class="flex hover:bg-gray-300 p-2 pr-5 rounded-s flex-grow-[6] flex-shrink transition-all no-underline"
                 href={`${
                     import.meta.env.DEV ? 'https://pkg.adfinis.com' : ''
                 }/${getLocalName()}`}
@@ -63,19 +63,17 @@ export default function Repo(props) {
                         })`,
                     }}
                 />
-                <div>
-                    <h3 class="text-black">{props.name}</h3>
-                    <p class="text-gray-700 whitespace-pre-wrap">
-                        Mirror for {props.name}
-                    </p>
-                    <p class="text-gray-700 whitespace-pre-wrap">
+                <div class="prose-p:m-0 prose-headings:m-0 prose-p:text-gray-700 prose-p:whitespace-pre-wrap">
+                    <h3>{props.name}</h3>
+                    <p>Mirror for {props.name}</p>
+                    <p>
                         Size:{' '}
                         <Show when={sizeData()} fallback={<>loading...</>}>
                             {' '}
                             {sizeData().size}{' '}
                         </Show>
                     </p>
-                    <p class="text-gray-700 whitespace-pre-wrap">
+                    <p>
                         Last Sync:{' '}
                         <Show when={sizeData()} fallback={<>loading...</>}>
                             {' '}
@@ -86,7 +84,7 @@ export default function Repo(props) {
             </a>
             <Show when={props.additional || props.help}>
                 <button
-                    class="text-adfinis-blue hover:text-adfinis-darkblue hover:bg-gray-300 p-3 z-10 rounded-e transition-all"
+                    class="text-adfinis-blue hover:text-adfinis-darkblue hover:bg-gray-300 p-4 z-10 rounded-e transition-all"
                     onClick={() => {
                         window.scrollTo(0, 0);
                         setModal(true);
@@ -100,22 +98,21 @@ export default function Repo(props) {
                     class="fixed z-20 w-full h-full opacity-25 bg-gray-600 inset-0"
                     onClick={() => setModal(false)}
                 />
-                <div class="absolute z-30 bg-white w-[90%] md:w-[85%] lg:w-[80%] p-4 rounded top-[50%] left-0 right-0 -translate-y-[50%] mx-auto">
-                    <h2 class="text-adfinis-blue">{props.name} help</h2>
+
+                <div class="absolute z-30 bg-white w-[90%] md:w-[85%] lg:w-[80%] p-4 rounded top-[50%] left-0 right-0 -translate-y-[50%] mx-auto flex flex-col prose max-w-none prose-p:my-0 prose-ul:my-0 prose-headings:text-adfinis-blue">
+                    <h2>{props.name} help</h2>
                     <Show when={props.help}>
                         <SolidMarkdown children={props.help} />
                     </Show>
                     <Show when={props.additional}>
-                        <h2 class="text-adfinis-blue mt-4">
-                            Additional information
-                        </h2>
+                        <h2>Additional information</h2>
                         <p>
                             You can find further information about our mirror on
                             the following site:
                         </p>
-                        <ul class="list-disc list-inside">
+                        <ul>
                             <li>
-                                <a role="link" href={props.additional}>
+                                <a href={props.additional}>
                                     {props.additional}
                                 </a>
                             </li>
