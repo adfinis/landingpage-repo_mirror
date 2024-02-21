24143230a505a0816f20e5b1b6002f3e7b7da287
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-02-21 11:02:06 +0100

    feat: use tailwinds typography for styling markdown

--- solid/tailwind.config.js
+++ solid/tailwind.config.js
@@ -1,25 +1,27 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  mode: "jit",
-  theme: {
-    fontFamily: {
-      sans: ["Source Sans Pro"],
-    },
-    extend: {
-      colors: {
-        adfinis: {
-          blue: "#2e4b98",
-          darkblue: "#1c2e5d",
-          green: "#2e987b",
-          grey: "#8b8b8c",
-          black: "#0f0f0f",
-          white: "#f8f7f7",
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    mode: 'jit',
+    theme: {
+        fontFamily: {
+            sans: ['Source Sans Pro'],
         },
-        black: "#0f0f0f",
-        white: "#f8f7f7",
-      },
+        extend: {
+            colors: {
+                adfinis: {
+                    blue: '#2e4b98',
+                    darkblue: '#1c2e5d',
+                    green: '#2e987b',
+                    grey: '#8b8b8c',
+                    black: '#0f0f0f',
+                    white: '#f8f7f7',
+                },
+                black: '#0f0f0f',
+                white: '#f8f7f7',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [typography()],
 };
