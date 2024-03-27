ba9718e55c2906a7f785f6d88de6fae9d4323ce4
Arthur Deierlein <arthur.deierlein@adfinis.com>
2024-03-27 14:05:57 +0100

    chore(deps): replace deprecated vite-plugin-fonts

--- solid/vite.config.js
+++ solid/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import { VitePluginFonts } from "vite-plugin-fonts";
-import solid from "vite-plugin-solid";
+import { defineConfig } from 'vite';
+import Unfonts from 'unplugin-fonts/vite';
+import solid from 'vite-plugin-solid';
 
 export default defineConfig({
-  server: {
-    port: 4200,
-  },
-  plugins: [
-    solid(),
-    VitePluginFonts({
-      google: { families: ["Source Sans Pro"] },
-    }),
-  ],
+    server: {
+        port: 4200,
+    },
+    plugins: [
+        solid(),
+        Unfonts({
+            google: { families: ['Source Sans Pro'] },
+        }),
+    ],
 });
